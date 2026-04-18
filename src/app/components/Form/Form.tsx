77ef983d13ae178c262bfaac0fef1d7153a5d78e
Hari <hariharan.s@digitalavenues.com>
2026-04-19 00:02:37 +0530

    mobile view fix .

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -37,114 +37,116 @@ export const Form = () => {
   const isValidRef = useRef(false)
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="mx-auto my-8 max-w-100 border  px-8 py-8 shadow-[1px_1px_0mpx_black] rounded-2xl"
-    >
-      <div className="flex justify-center items-center">
-        <h2 className="text-3xl">Book A Free Trial</h2>
-      </div>
-      <Input
-        name="firstname"
-        labelName="PARENT NAME"
-        required
-        register={register}
-        errors={errors}
-        requiredErrorMsg="This Field is Required"
-        min={3}
-      />
-      <Input
-        name="studentname"
-        labelName="CHILD NAME"
-        required
-        register={register}
-        errors={errors}
-        requiredErrorMsg="This Field is Required"
-        min={3}
-      />
-      <Input
-        name="email"
-        labelName="EMAIL ADDRESS"
-        required
-        register={register}
-        errors={errors}
-        requiredErrorMsg="This Field is Required"
-        type="email"
-        pattern={/^[^\s@]+@[^\s@]+\.[^\s@]+$/}
-        emailError="Invalid Email Address"
-      />
-      <div>
-        <label
-          className={`w-full px-1 -top-2.5 left-3 bg-white text-[0.75rem] font-normal ${errors?.phone?.message ? 'text-red-600' : ''}`}
-        >
-          PHONE NUMBER{' '}
-          <span className="ml-0.5 text-red-600 text-[0.85rem]">*</span>
-        </label>
-
-        <Controller
-          name="phone"
-          control={control}
-          // rules={{
-          //   required: 'This Field is required',
-          //   // validate: () => {
-          //   //   if (!isValidRef.current) {
-          //   //     return 'Enter a valid phone number'
-          //   //   }
-          //   //   return true
-          //   // },
-          // }}
-          render={({ field }) => (
-            <div>
-              <IntlTelInput
-                {...field}
-                initialCountry="auto"
-                strictMode
-                separateDialCode
-                formatOnDisplay
-                usePreciseValidation
-                value={field.value} // ✅ bind value
-                onChangeNumber={(number: string) => {
-                  field.onChange(number) // ✅ send value to RHF
-                }}
-                // onChangeValidity={(isValid: boolean) => {
-                //   isValidRef.current = isValid // ✅ track validity
-                //   console.log('isValidRef', isValidRef.current)
-                //   // if (!isValidRef.current) {
-                //   //   setError('phone', {
-                //   //     type: 'manual',
-                //   //     message: 'Dont Forget Your Username Should Be Cool!',
-                //   //   })
-                //   // }
-                // }}
-                geoIpLookup={(callback) => {
-                  fetch('https://ipapi.co/json/')
-                    .then((res) => res.json())
-                    .then((data: any) => {
-                      console.log('')
-                      callback(data.country_code?.toLowerCase()) // e.g., "in"
-                    })
-                    .catch(() => {
-                      callback('ae') // fallback (India)
-                    })
-                }}
-              />
-
-              {errors.phone && (
-                <p className="mt-0.5 text-red-600 text-[0.75rem]">
-                  {errors.phone.message as string}
-                </p>
-              )}
-            </div>
-          )}
-        />
-      </div>
-
-      <button
-        type="submit"
-        className="w-full mt-8 shadow-[3px_3px_0px_black] border-1 border-black xl:px-8 xl:py-3 px-6 py-2 text-white rounded text-[1rem] font-extrabold bg-[#2b51ff]"
+    <div className="py-8 px-4">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mx-auto  my-8 max-w-100 border  px-8 py-8 shadow-[1px_1px_0mpx_black] rounded-2xl"
       >
-        Book A Free Trial
-      </button>
-    </form>
+        <div className="flex justify-center items-center">
+          <h2 className="text-3xl">Book A Free Trial</h2>
+        </div>
+        <Input
+          name="firstname"
+          labelName="PARENT NAME"
+          required
+          register={register}
+          errors={errors}
+          requiredErrorMsg="This Field is Required"
+          min={3}
+        />
+        <Input
+          name="studentname"
+          labelName="CHILD NAME"
+          required
+          register={register}
+          errors={errors}
+          requiredErrorMsg="This Field is Required"
+          min={3}
+        />
+        <Input
+          name="email"
+          labelName="EMAIL ADDRESS"
+          required
+          register={register}
+          errors={errors}
+          requiredErrorMsg="This Field is Required"
+          type="email"
+          pattern={/^[^\s@]+@[^\s@]+\.[^\s@]+$/}
+          emailError="Invalid Email Address"
+        />
+        <div>
+          <label
+            className={`w-full px-1 -top-2.5 left-3 bg-white text-[0.75rem] font-normal ${errors?.phone?.message ? 'text-red-600' : ''}`}
+          >
+            PHONE NUMBER{' '}
+            <span className="ml-0.5 text-red-600 text-[0.85rem]">*</span>
+          </label>
+
+          <Controller
+            name="phone"
+            control={control}
+            // rules={{
+            //   required: 'This Field is required',
+            //   // validate: () => {
+            //   //   if (!isValidRef.current) {
+            //   //     return 'Enter a valid phone number'
+            //   //   }
+            //   //   return true
+            //   // },
+            // }}
+            render={({ field }) => (
+              <div>
+                <IntlTelInput
+                  {...field}
+                  initialCountry="auto"
+                  strictMode
+                  separateDialCode
+                  formatOnDisplay
+                  usePreciseValidation
+                  value={field.value} // ✅ bind value
+                  onChangeNumber={(number: string) => {
+                    field.onChange(number) // ✅ send value to RHF
+                  }}
+                  // onChangeValidity={(isValid: boolean) => {
+                  //   isValidRef.current = isValid // ✅ track validity
+                  //   console.log('isValidRef', isValidRef.current)
+                  //   // if (!isValidRef.current) {
+                  //   //   setError('phone', {
+                  //   //     type: 'manual',
+                  //   //     message: 'Dont Forget Your Username Should Be Cool!',
+                  //   //   })
+                  //   // }
+                  // }}
+                  geoIpLookup={(callback) => {
+                    fetch('https://ipapi.co/json/')
+                      .then((res) => res.json())
+                      .then((data: any) => {
+                        console.log('')
+                        callback(data.country_code?.toLowerCase()) // e.g., "in"
+                      })
+                      .catch(() => {
+                        callback('ae') // fallback (India)
+                      })
+                  }}
+                />
+
+                {errors.phone && (
+                  <p className="mt-0.5 text-red-600 text-[0.75rem]">
+                    {errors.phone.message as string}
+                  </p>
+                )}
+              </div>
+            )}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full mt-8 shadow-[3px_3px_0px_black] border-1 border-black xl:px-8 xl:py-3 px-6 py-2 text-white rounded text-[1rem] font-extrabold bg-[#2b51ff]"
+        >
+          Book A Free Trial
+        </button>
+      </form>
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <PainPointSection />
       <GoogleReview />
+
       <Form />
     </div>
   )
