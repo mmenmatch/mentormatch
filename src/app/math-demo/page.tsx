9d02d6aa9c30e1981752518d8a2e97bde9444e9f
Hari <hariharan.s@digitalavenues.com>
2026-06-04 14:04:53 +0530

    Open Graph Image .

--- src/app/math-demo/page.tsx
+++ src/app/math-demo/page.tsx
@@ -8,6 +8,19 @@ export const metadata: Metadata = {
   title: 'Math | Mentor Match',
   description:
     'Learn more about Mentor Match and how we help students connect with expert tutors.',
+  openGraph: {
+    title: 'Math | Mentor Match',
+    description:
+      'Learn more about Mentor Match and how we help students connect with expert tutors.',
+    images: [
+      {
+        url: 'https://cdn.prod.website-files.com/645c85e0705da20f26b63b8e/6a21389fc30b68ec4cafbe77_OG.webp', // absolute URL
+        width: 1200,
+        height: 630,
+        alt: 'Math | Mentor Match',
+      },
+    ],
+  },
 }
 export default function page() {
   return (
