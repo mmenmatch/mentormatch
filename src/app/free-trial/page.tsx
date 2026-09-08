a780a61cdd4fd45d33cda2ac38f432337a77f6d3
Hari <hariharan.s@digitalavenues.com>
2026-09-08 12:24:03 +0530

    view issue

--- src/app/free-trial/page.tsx
+++ src/app/free-trial/page.tsx
@@ -8,7 +8,7 @@ const freeTrialPage = () => {
   return (
     <div className="w-full flex flex-col">
       <NavBar whatsapp />
-      <div className="w-full min-h-[calc(100vh-80px)] bg-[linear-gradient(90deg,#006CFF_62%,#049FF3_100%)] flex flex-col md:py-16 py-4 gap-16 relative">
+      <div className="w-full min-h-[calc(100vh-80px)] bg-[linear-gradient(90deg,#006CFF_62%,#049FF3_100%)] flex flex-col justify-center md:py-16 py-4 gap-16 relative">
         <div className="w-full flex md:flex-row flex-col justify-center items-end px-4 md:gap-8 gap-4 ">
           <div className="free-trial-hero-section md:w-[40%] w-full flex flex-col justify-start items-center">
             <h1 className="text-white text-left md:text-[3rem] text-[7vw] leading-[140%] text-center">
@@ -29,7 +29,7 @@ const freeTrialPage = () => {
             <FreeTrialForm />
           </div>
         </div>
-        <div className="w-full flex flex-col gap-4 justify-center items-center md:absolute relative bottom-5">
+        <div className="w-full flex flex-col gap-4 justify-center items-center  relative bottom-5">
           <Image src={RatingsBadge} alt={'mentor match'} width={300} />
           <p className="text-white text-[1rem]">© 2025 Mentor Match. All rights reserved.</p>
         </div>
